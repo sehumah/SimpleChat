b1574993825d275e10263f83bb00f438aeb562b4
sehumah <sehumah@protonmail.com>
2022-03-20 02:44:03 -0500

    add functionality for logging in with anonymous parse user

--- app/src/main/java/com/lab5/simplechat/ChatActivity.kt
+++ app/src/main/java/com/lab5/simplechat/ChatActivity.kt
@@ -2,10 +2,41 @@ package com.lab5.simplechat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.parse.ParseAnonymousUtils
+import com.parse.ParseUser
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        // User login
+        if (ParseUser.getCurrentUser() != null) { // start with existing user
+            startWithCurrentUser()
+        }
+        else { // If not logged in, login as a new anonymous user
+            login()
+        }
+    }
+
+    // Get the userId from the cached currentUser object
+    private fun startWithCurrentUser() {
+        // TODO: implement functionality
+    }
+
+    // Create an anonymous user using ParseAnonymousUtils and set sUserId
+    private fun login() {
+        ParseAnonymousUtils.logIn { user, e ->
+            if (e != null) {
+                Log.e(TAG, "Anonymous login failed: ", e)
+            } else {
+                startWithCurrentUser()
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG: String = "ChatActivity"
     }
 }
