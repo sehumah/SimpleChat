1cff609f78cd1eec98bccc9eacdcd32d6a2f44b9
sehumah <sehumah@protonmail.com>
2022-03-21 15:23:51 -0500

    create chat adapter class to render messages by prefilling appropriate info

---
+++ app/src/main/java/com/lab5/simplechat/ChatAdapter.kt
@@ -0,0 +1,10 @@
+package com.lab5.simplechat
+
+/**
+ * This is a custom list adapter class which provides data to RecyclerView.
+ * It renders the message_incoming.xml or message_outgoing.xml in list by pre-filling appropriate information.
+ */
+
+class ChatAdapter {
+
+}
