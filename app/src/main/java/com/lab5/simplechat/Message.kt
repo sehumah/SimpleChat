5b07149301f9d3433680700a3cdcd322edd57634
sehumah <sehumah@protonmail.com>
2022-03-21 15:10:29 -0500

    add decorator to class object

--- app/src/main/java/com/lab5/simplechat/Message.kt
+++ app/src/main/java/com/lab5/simplechat/Message.kt
@@ -1,5 +1,6 @@
 package com.lab5.simplechat
 
+import com.parse.ParseClassName
 import com.parse.ParseObject
 
 
@@ -7,6 +8,7 @@ import com.parse.ParseObject
  * This model class will provide message data for the RecyclerView, and will be used to retrieve and save messages to Parse.
  */
 
+@ParseClassName("Message")
 class Message : ParseObject () {
     val USER_ID_KEY: String = "userId"
     val BODY_KEY: String = "body"
