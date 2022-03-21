ab0b98c322d311886c13516396de5b479774ff19
sehumah <sehumah@protonmail.com>
2022-03-21 15:09:21 -0500

    create message model/class which will extend from ParseObject

---
+++ app/src/main/java/com/lab5/simplechat/Message.kt
@@ -0,0 +1,29 @@
+package com.lab5.simplechat
+
+import com.parse.ParseObject
+
+
+/**
+ * This model class will provide message data for the RecyclerView, and will be used to retrieve and save messages to Parse.
+ */
+
+class Message : ParseObject () {
+    val USER_ID_KEY: String = "userId"
+    val BODY_KEY: String = "body"
+
+    fun setUserId(userId: String) {
+        put(USER_ID_KEY, userId)
+    }
+
+    fun setBody(body: String) {
+        put(BODY_KEY, body)
+    }
+
+    fun getUserId(): String? {
+        return getString(USER_ID_KEY)
+    }
+
+    fun getBody(): String? {
+        return getString(BODY_KEY)
+    }
+}
