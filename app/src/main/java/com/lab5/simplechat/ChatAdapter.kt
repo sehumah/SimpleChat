cce506e9b88ea7bc124e4f82f9ba2f5503c32197
sehumah <sehumah@protonmail.com>
2022-03-21 16:07:16 -0500

    define implementations for incoming and outgoing message view holders

--- app/src/main/java/com/lab5/simplechat/ChatAdapter.kt
+++ app/src/main/java/com/lab5/simplechat/ChatAdapter.kt
@@ -3,6 +3,8 @@ package com.lab5.simplechat
 import android.content.Context
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 
@@ -14,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView
 class ChatAdapter(private val context: Context, private val userId: String, private val messages: List<Message>) :
     RecyclerView.Adapter<ChatAdapter.MessageViewHolder> {
 
+    // define the class as abstract meaning it can't have any instances
     abstract class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder (itemView) {
 
         fun MessageViewHolder(itemView: View) {
@@ -23,15 +26,50 @@ class ChatAdapter(private val context: Context, private val userId: String, priv
         abstract fun bindMessage(message: Message): Void
     }
 
+
+    class IncomingMessageViewHolder(itemView: View) : MessageViewHolder(itemView) {
+        private var ivProfileOther: ImageView?
+        private var tvBody: TextView?
+        private var tvName: TextView?
+
+        init {
+            super.itemView
+            ivProfileOther = itemView.findViewById(R.id.iv_profile_other)
+            tvBody = itemView.findViewById(R.id.tv_body)
+            tvName = itemView.findViewById(R.id.tv_name)
+        }
+
+        override fun bindMessage(message: Message): Void {
+            TODO("implement later")
+        }
+    }
+
+
+    class OutgoingMessageViewHolder(itemView: View) : MessageViewHolder(itemView) {
+        private var ivProfileMe: ImageView? = null
+        private var tvBody: TextView? = null
+
+        init {
+            super.itemView
+            ivProfileMe = itemView.findViewById(R.id.iv_profile_me)
+            tvBody = itemView.findViewById(R.id.tv_body)
+        }
+
+        override fun bindMessage(message: Message): Void {
+            TODO("implement later")
+        }
+    }
+
+
     private val MESSAGE_OUTGOING: Int = 123
     private val MESSAGE_INCOMING: Int = 321
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ??? {
-        // TODO("Not yet implemented")
+        TODO("Not yet implemented")
     }
 
     override fun onBindViewHolder(holder: ???, position: Int) {
-        // TODO("Not yet implemented")
+        TODO("Not yet implemented")
     }
 
     override fun getItemCount(): Int {
