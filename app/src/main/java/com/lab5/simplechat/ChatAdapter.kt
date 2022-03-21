0c715c3026adcc0da21a1a62849544dfce2a6545
sehumah <sehumah@protonmail.com>
2022-03-21 15:33:42 -0500

    create a ChatAdapter that extends RecyclerView.Adapter

--- app/src/main/java/com/lab5/simplechat/ChatAdapter.kt
+++ app/src/main/java/com/lab5/simplechat/ChatAdapter.kt
@@ -1,10 +1,25 @@
 package com.lab5.simplechat
 
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
 /**
  * This is a custom list adapter class which provides data to RecyclerView.
  * It renders the message_incoming.xml or message_outgoing.xml in list by pre-filling appropriate information.
  */
 
-class ChatAdapter {
+class ChatAdapter(private val context: Context, private val userId: String, private val messages: List<Message>) : RecyclerView.Adapter<ChatAdapter.MessageViewHolder> {
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ??? {
+        // TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ???, position: Int) {
+        // TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return messages.size
+    }
 }
